43f391e55e95be17b7a9985092eddb134f6c48b4
Abdul Moiz <moiza8994@gmail.com>
2021-09-19 14:59:22 +0500

    add the MissionList data all the list in array of objects and query this in separate Comp

--- src/components/MissionList/MissionList.tsx
+++ src/components/MissionList/MissionList.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {MissionListQuery} from '../../generated/graphql'
+
+interface Props{
+    data: MissionListQuery | undefined
+}
 
 
-
-export const MissionList = () => {
+export const MissionList: React.FC<Props> = ({data}) => {
+    console.log(data);
+    
     return (
         <div>
             
--- src/components/MissionList/index.tsx
+++ src/components/MissionList/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import {useMissionListQuery} from '../../generated/graphql'
+import {MissionList} from './MissionList'
 
-export const index = () => {
+export const MissionContainer = () => {
+    const {data,loading,error}=useMissionListQuery()
+
+    if(loading){
+        return <h1>The Data is Loading</h1>
+    }
+    if(error){
+        return <h1>There is an Error</h1>
+    }
+     console.log(data);
+     
     return (
         <div>
-            
+            <MissionList  data={data}/>
         </div>
     )
 }
